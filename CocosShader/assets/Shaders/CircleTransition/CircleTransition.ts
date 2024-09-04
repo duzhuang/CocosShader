eadf6804ff1e3755263d9536b277d0caea52819c
duzhuang <1148299682@qq.com>
2024-09-04 18:07:51 +0800

    edit circleTransition shader

--- CocosShader/assets/Shaders/CircleTransition/CircleTransition.ts
+++ CocosShader/assets/Shaders/CircleTransition/CircleTransition.ts
@@ -1,28 +1,47 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
-const {ccclass, property} = cc._decorator;
+const { ccclass, property, menu, executeInEditMode } = cc._decorator;
 
 @ccclass
-export default class NewClass extends cc.Component {
-
-    @property(cc.Label)
-    label: cc.Label = null;
-
-    @property
-    text: string = 'hello';
-
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {}
-
-    start () {
-
+@menu('Shaders/2D/CircleTransition')
+@executeInEditMode
+export default class CircleTransition extends cc.Component {
+    @property()
+    _speed = 1;
+    @property({ type: cc.Float, min: 0.1 })
+    set speed(value) {
+        this._speed = value;
+        this.setSpeed();
+    }
+    get speed() {
+        return this._speed;
     }
 
-    // update (dt) {}
+    @property({ type: cc.Texture2D })
+    subTexture: cc.Texture2D = null;
+
+    private m_sprite: cc.Sprite = null!;
+    private m_material: cc.Material = null!;
+
+    protected start(): void {
+        this.m_sprite = this.getComponent(cc.Sprite)!;
+        this.m_material = this.m_sprite.getMaterial(0)!;
+        this.showWarn();
+        this.setSpeed();
+        this.setSubTexture();
+    }
+
+    showWarn(){
+        //@ts-ignore
+        if (String(this.m_material.material._name)!== "circleTransition_material") {
+            cc.error("节点：", this.name, "材质的名字不是circleTransition_material");
+            return;
+        }
+    }
+
+    setSpeed(){
+        this.m_material.setProperty('speed', this.speed);
+    }
+
+    setSubTexture(){
+        this.m_material.setProperty('subTexture', this.subTexture);
+    }
 }

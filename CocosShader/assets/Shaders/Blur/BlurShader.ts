f9bcd554fbe6478519c96eec2b4e7b1f7694a87f
zhangdaxing <1148299682@qq.com>
2024-09-09 13:41:08 +0800

    edit blur shader

--- CocosShader/assets/Shaders/Blur/BlurShader.ts
+++ CocosShader/assets/Shaders/Blur/BlurShader.ts
@@ -1,28 +1,57 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
-const {ccclass, property} = cc._decorator;
-
+const { ccclass, property, executeInEditMode, menu } = cc._decorator;
 @ccclass
-export default class NewClass extends cc.Component {
+@menu('Shaders/2D/BlurShader')
+@executeInEditMode
+export default class BlurShader extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
+    @property()
+    _blur: boolean = false;
 
-    @property
-    text: string = 'hello';
-
-    // LIFE-CYCLE CALLBACKS:
-
-    // onLoad () {}
-
-    start () {
-
+    @property({ type: cc.Boolean })
+    set blur(value: boolean) {
+        this._blur = value;        
+        this.setBlur();
+    }
+    get blur(): boolean {
+        return this._blur;
     }
 
-    // update (dt) {}
+    @property()
+    _uvoffset = 0.05;
+
+    @property({ type: cc.Float, min: 0, max: 1, visible() { return this.blur; } })
+    set uvoffset(value: number) {
+        this._uvoffset = value;
+        this.setuvOffset();
+    }
+    get uvoffset(): number {
+        return this._uvoffset;
+    }
+
+    private m_sprite: cc.Sprite = null!;
+    private m_material: cc.Material = null!;
+
+    protected start(): void {
+        this.m_sprite = this.getComponent(cc.Sprite)!;
+        this.m_material = this.m_sprite.getMaterial(0)!;
+        this.showWarn();
+        this.setBlur();
+        this.setuvOffset();
+    }
+
+    showWarn() {
+        //@ts-ignore
+        if (String(this.m_material.material._name) !== "blur_material") {
+            cc.error("节点：", this.name, "材质的名字不是", "blur_material");
+            return;
+        }
+    }
+
+    setBlur() {
+        this.m_material.define("USE_BLUR", this.blur, 0);
+    }
+
+    setuvOffset() {
+        this.m_material.setProperty("uvoffset", this.uvoffset);
+    }
 }
